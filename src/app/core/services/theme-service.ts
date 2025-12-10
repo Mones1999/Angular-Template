76c61b040aa788cb4aaa3e8de5c8ecddc1f9ce7f
Munes.AbuNsair <Munes.AbuNsair@globitel.com>
2025-12-10 14:51:55 +0300

    feat: Add theme service and header component with global SCSS styling.

---
+++ src/app/core/services/theme-service.ts
@@ -0,0 +1,31 @@
+import { DOCUMENT } from '@angular/common';
+import { Injectable, computed, effect, inject, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  private document = inject(DOCUMENT);
+  #isDarkTheme = signal<boolean>(this.getInitialTheme());
+  public isDarkTheme = computed(() => this.#isDarkTheme());
+
+  constructor() {
+    effect(() => {
+      const isDark = this.#isDarkTheme();
+      if (isDark) {
+        this.document.documentElement.classList.add('my-app-dark');
+      } else {
+        this.document.documentElement.classList.remove('my-app-dark');
+      }
+      localStorage.setItem('app-theme', isDark ? 'dark' : 'light');
+    });
+  }
+
+  public toggleTheme(): void {
+    this.#isDarkTheme.update((current) => !current);
+  }
+
+  private getInitialTheme(): boolean {
+    return localStorage.getItem('app-theme') === 'dark';
+  }
+}
