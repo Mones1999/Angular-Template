8750b880c2b406874c3c9fb3a83c888428b6944a
Munes.AbuNsair <Munes.AbuNsair@globitel.com>
2026-04-02 15:02:39 +0300

    feat(environment): add production environment configuration and file replacements

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://localhost:9009/SDK_API/api',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:9009/SDK_API/api',
+};
